bd5df3bf98724de1e0369d313d0a1cf4691842ae
thbdias <thiagobalbino91@gmail.com>
2019-10-28 10:29:12 -0300

    read json

--- rest_tutorial/src/main/java/io/codementor/gtommee/rest_tutorial/controllers/PetsController.java
+++ rest_tutorial/src/main/java/io/codementor/gtommee/rest_tutorial/controllers/PetsController.java
@@ -5,10 +5,14 @@ import io.codementor.gtommee.rest_tutorial.repositories.PetsRepository;
 import org.bson.types.ObjectId;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -84,6 +88,55 @@ public class PetsController {
             e.printStackTrace();
             return "erro";
         }
+    }
+
+
+    @RequestMapping(value = "/read_json", method = RequestMethod.GET)
+    public String readJson() {
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("C:\\Users\\balbinth\\Documents\\employees.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+
+            JSONArray employeeList = (JSONArray) obj;
+            System.out.println(employeeList);
+
+            //Iterate over employee array
+            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+
+            return "lido";
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "erro 1";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "erro 2";
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "erro 3";
+        }
+    }
+
+    private void parseEmployeeObject(JSONObject employee)
+    {
+        //Get employee object within list
+        JSONObject employeeObject = (JSONObject) employee.get("employee");
+
+        //Get employee first name
+        String firstName = (String) employeeObject.get("firstName");
+        System.out.println(firstName);
+
+        //Get employee last name
+        String lastName = (String) employeeObject.get("lastName");
+        System.out.println(lastName);
+
+        //Get employee website name
+        String website = (String) employeeObject.get("website");
+        System.out.println(website);
     }
 
 }
