119ceb3604fefc1ba4cd6e0e6d61407fc44c8d71
thbdias <thiagobalbino91@gmail.com>
2019-10-30 10:59:17 -0300

    add entities

---
+++ rest_tutorial/src/main/java/io/codementor/gtommee/rest_tutorial/models/Company.java
@@ -0,0 +1,74 @@
+package io.codementor.gtommee.rest_tutorial.models;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection="company")
+public class Company {
+    @Id
+    private int id;
+    private String name;
+
+    private List<Product> products;
+
+    private Contact contact;
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setProducts(List<Product> products){
+        this.products = products;
+    }
+
+    public List<Product> getProducts(){
+        return this.products;
+    }
+
+    public void setContact(Contact contact){
+        this.contact = contact;
+    }
+
+    public Contact getContact(){
+        return this.contact;
+    }
+
+    public Company(int id, String name, List<Product> products, Contact contact){
+        this.id = id;
+        this.name = name;
+        this.products = products;
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        String jsonString = "";
+        try {
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            jsonString = mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return jsonString;
+    }
+}
--- rest_tutorial/src/main/java/io/codementor/gtommee/rest_tutorial/repositories/PetsRepository.java
+++ rest_tutorial/src/main/java/io/codementor/gtommee/rest_tutorial/repositories/PetsRepository.java
@@ -1,9 +1,16 @@
 package io.codementor.gtommee.rest_tutorial.repositories;
 
+import io.codementor.gtommee.rest_tutorial.models.Company;
 import io.codementor.gtommee.rest_tutorial.models.Pets;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface PetsRepository extends MongoRepository<Pets, String> {
-    Pets findBy_id(ObjectId _id);
+    List<Company> findByName(String name);
+
+    @Query("{'contact.address': ?0}")
+    List<Company> findByAddress(String address);
 }
