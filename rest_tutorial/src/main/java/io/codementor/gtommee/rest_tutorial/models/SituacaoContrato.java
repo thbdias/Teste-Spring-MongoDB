cc5919dfa954c0885b6028529b99d3c028cfb9bd
thbdias <thiagobalbino91@gmail.com>
2019-11-26 15:31:34 -0300

    add campo situacaoContrato

--- rest_tutorial/src/main/java/io/codementor/gtommee/rest_tutorial/models/SituacaoContrato.java
+++ rest_tutorial/src/main/java/io/codementor/gtommee/rest_tutorial/models/SituacaoContrato.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class SituacaoContrato {
     @JsonProperty("diaVenc") //chave dentro do json
     private int diaVencimento;
+    @JsonProperty("dtPriAber") //chave dentro do json
+    private String dataPrimeiraPrestacaoAberta; //yyyyMM
 
     public int getDiaVencimento() {
         return diaVencimento;
@@ -12,5 +14,13 @@ public class SituacaoContrato {
 
     public void setDiaVencimento(int diaVencimento) {
         this.diaVencimento = diaVencimento;
+    }
+
+    public String getDataPrimeiraPrestacaoAberta() {
+        return dataPrimeiraPrestacaoAberta;
+    }
+
+    public void setDataPrimeiraPrestacaoAberta(String dataPrimeiraPrestacaoAberta) {
+        this.dataPrimeiraPrestacaoAberta = dataPrimeiraPrestacaoAberta;
     }
 }
